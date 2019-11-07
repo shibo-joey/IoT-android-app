09ac283ddf065056546abedf1ed777a777cee750
Your Name <you@example.com>
2019-11-07 15:48:35 -0500

    initialized

---
+++ app/src/main/java/com/example/locklockdoor/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.locklockdoor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
+    }
+
+    public void jsonParse(){
+        String url = "https://api.thingspeak.com/channels/886152/feeds.json?api_key=JDM0TOWAHQDDVW7Z&results=2";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("feeds");
+
+                                JSONObject now_time = jsonArray.getJSONObject(1);
+                                String now =  now_time.getString("field1");
+
+                                JSONObject last_time = jsonArray.getJSONObject(0);
+                                String last =  last_time.getString("field1");
+
+                                mTextViewResult.append("Status Now:" + now + "\n" + "Last Status:" + last + "\n\n");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
+}
